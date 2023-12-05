8ff2707c449c089bab75239b7c6ad422c59eec9d
Changolaxtra <dan.rojasx@gmail.com>
2023-12-04 23:47:13 -0600

    fix: repository

--- src/main/java/com/bank/payments/api/repository/CreditCardRepository.java
+++ src/main/java/com/bank/payments/api/repository/CreditCardRepository.java
@@ -5,27 +5,27 @@ import com.bank.payments.api.model.CreditCard;
 import java.util.Map;
 
 // TODO Add implementation of the methods to store, read and update the credit card and balance.
-public class CreditCardRepository implements BankRepository<String, CreditCard> {
+public class CreditCardRepository implements BankRepository<CreditCard,String> {
 
     private Map<String, CreditCard> numberToCreditCard;
 
     @Override
-    public boolean save(CreditCard key, String entity) {
+    public boolean save(String key, CreditCard entity) {
         return false;
     }
 
     @Override
-    public String find(CreditCard key) {
+    public CreditCard find(String key) {
         return null;
     }
 
     @Override
-    public void update(CreditCard key, String entity) {
+    public void update(String key, CreditCard entity) {
 
     }
 
     @Override
-    public boolean exists(CreditCard key) {
+    public boolean exists(String key) {
         return false;
     }
 }
